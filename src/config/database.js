cdf6dfd46b7ea092bdf49c16adc062388e02d7ed
Mustafa Tarek <mustafatarek104@gmail.com>
2026-08-05 05:35:12 +0300

    Modifying database config

--- src/config/database.js
+++ src/config/database.js
@@ -5,6 +5,9 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.on("error", (err) => {
